bd6987fa4cc89a02685d909b9f68c57ac098c1ce
ToblerX <super.yaroslav05@gmail.com>
2025-01-18 16:54:34 +0100

    Deleted extra services from signup and login

---
+++ BookWorm/Migrations/20250118155335_CrookedHouseChangeDesc.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookWorm.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrookedHouseChangeDesc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Description",
+                value: "A classic detective novel with an unexpected ending.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Description",
+                value: "A classic detective novel featuring Hercule Poirot.");
+        }
+    }
+}
